a1c0b1cf1a8b125f745e14b15d9c880dc2770ff9
LuiiFC <luiscardoso2311@gmail.com>
2026-06-04 20:50:51 -0300

    feat: sistema de curtidas para empresas parceiras

--- src/routes/projetos.js
+++ src/routes/projetos.js
@@ -12,5 +12,7 @@ router.get('/:id', auth, ctrl.buscarPorId);
 router.post('/:id/upload', auth, upload.single('arquivo'), ctrl.uploadArquivo);
 router.delete('/:id', auth, apenas('coordenador'), ctrl.deletar);
 router.patch('/:id/categoria', auth, apenas('coordenador'), ctrl.vincularCategoria);
+router.post('/:id/curtir', auth, apenas('empresa_parceira'), ctrl.curtir);
+router.get('/:id/curtidas', auth, ctrl.getCurtidas);
 
 module.exports = router;
